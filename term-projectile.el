8be23255fb170cdcb332eebbf299d76eb44bcfe6
Ivan Malison <IvanMalison@gmail.com>
2016-11-06 00:51:16 -0700

    Add emacs 24 dependency to term-projectile

--- term-projectile.el
+++ term-projectile.el
@@ -6,7 +6,7 @@
 ;; Keywords: term manager projectile
 ;; URL: https://www.github.com/IvanMalison/term-manager
 ;; Version: 0.1.0
-;; Package-Requires: ((term-manager "0.1.0") (projectile "0.13.0"))
+;; Package-Requires: ((emacs "24") (term-manager "0.1.0") (projectile "0.13.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
